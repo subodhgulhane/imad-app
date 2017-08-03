0bd343ef746483e701910f9a332bfb170cb8bd3d
subodhgulhane <subodhgulhane@gmail.com>
2017-08-03 20:48:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.get('/ui/madi.png', function (req, res) {
 
 var port = 80;
 app.listen(port, function () {
-  console.log(`IMAD NPTEL app listening on port ${port}!`);
+  console.log(`IMAD NPTEL subodh gulhane app listening on port ${port}!`);
 });
